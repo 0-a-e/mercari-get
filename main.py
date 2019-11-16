6dfe8c5a17fbd551286c3dc4a77bb1cdd5401df0
uuparupa <uparupa@uuparupa-no-MacBook-Air.local>
2019-11-17 07:46:46 +0900

    修復中

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import csv
+import firebase_admin
+from firebase_admin import credentials,db
 import os
 import pandas as pd
 import difflib as diff
 count = 0
-def CSVDIFF():
-    df1 = pd.read_csv('./main.csv')
-    df2 = pd.read_csv('./second.csv')
-    '''
-     def make_lines_set(txt):
+'''
+    def make_lines_set(txt):
         f = open(txt)
         lines = f.readlines()
         sets = set(lines)
@@ -19,7 +18,16 @@ def CSVDIFF():
     results = bigs.difference(smalls)
     for result in results:
         print(result)
-    '''
+'''
+cred = credentials.Certificate("mercari-price-firebase-adminsdk-hi90f-b7e0d246d7.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://mercari-price.firebaseio.com/',
+    'databaseAuthVariableOverride': {
+        'uid': 'my-service-worker'
+    }
+    })
+users_ref = db.reference('/data')
+dbres = users_ref.get()
 with open('./index.html', 'w') as f:
     print('<head><meta name="viewport" content="width=device-width"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script></head><div class="container"><div class="row">',file=f)
     f.close()
@@ -78,17 +86,24 @@ def get_item(driver):
             print('<img src="' + img + '"></img>',file=f)
             print(price,file=f)
             print('</div>',file=f)
+            if dbres[count]["title"] == title:
+                print("match")
+            else:
+                print(dbres[count]["title"])
+                print(title)
+                print("dont match")
             f.close()
-        if os.path.isfile('./main.csv'):
-            with open('second.csv', 'a') as f:
-                writer = csv.writer(f)
-                writer.writerow([title,price,link,img])
-                f.close()
-        else:
             with open('main.csv', 'a') as f:
                 writer = csv.writer(f)
                 writer.writerow([title,price,link,img])
                 f.close()
+        users_ref.child(str(count)).set({
+        'title': title,
+        'price': price,
+        'link': link,
+        'img': img
+        })
+
     if count > search_limit:
         return
 
